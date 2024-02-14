6d87913b568e6bf2f2ef830860ceb47410523c30
ZigraFera <78167564+ZigraFera@users.noreply.github.com>
2024-02-14 20:49:50 +0000

    Autorizācijas vizuāls un primitīvi funkcionāls  pogu un atsauču pamats

--- main.py
+++ main.py
@@ -2,8 +2,34 @@ import json
 import sqlite3
 import random
 from flask import Flask, redirect, render_template, request, url_for
+
+
+def nolasit_teksta_datus(vietnes_vieta):
+  teksta_fails = open("Dati/teksti.json", encoding="utf8")
+  vietnes_teksti = json.load(teksta_fails)
+  teksta_fails.close()
+  if vietnes_vieta == "sakums":
+    return vietnes_teksti.get("sakums")
+  elif vietnes_vieta == "registresanas":
+    return vietnes_teksti.get("registresanas")
+  elif vietnes_vieta == "ielogosanas":
+    return vietnes_teksti.get("ielogosanas")
+    
+
 app = Flask("app")
+
 @app.route("/")
-def index():
-  return render_template("sakums.html")
+def sakums():
+  teksti = nolasit_teksta_datus("sakums")
+  return render_template("sakums.html",teksti=teksti)
+
+@app.route("/registresanas", methods=["GET", "POST"])
+def registresanas():
+  teksti = nolasit_teksta_datus("registresanas")
+  return render_template("registresanas.html",teksti=teksti)
+  
+@app.route("/ielogosanas", methods=["GET", "POST"])
+def ielogosanas():
+  teksti = nolasit_teksta_datus("ielogosanas")
+  return render_template("ielogosanas.html",teksti=teksti)
 app.run(host="0.0.0.0", port=8080)
